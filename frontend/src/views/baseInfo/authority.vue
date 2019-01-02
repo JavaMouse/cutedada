748c177109c7558e7afaf7d7e5c3fd4bbf15918b
chennan <chennan@chennandeMacBook-Pro-2.local>
2019-01-02 17:47:43 +0800

    Fix  checkbox

--- frontend/src/views/baseInfo/authority.vue
+++ frontend/src/views/baseInfo/authority.vue
@@ -2,31 +2,15 @@
     <el-container>
         <el-main class="main">
             <el-form :label-position="labelPosition" label-width="0px" :model="authorityForm" class="authForm">
-                <el-form-item v-for="(item,index) in checkList" :key="index">
-                    <div>echart{{item}}:</div>
-                    <el-checkbox></el-checkbox>
-                    <span>查看</span>
-                    <el-checkbox></el-checkbox>
-                    <span>编辑</span>
+                <el-form-item v-for="(item,index) in formList" :key="index">
+                    <div>echart{{index+1}}:</div>
+                    <el-checkbox label="查看" :checked="item[0]" @change="test(index,0)"></el-checkbox>
+                    <el-checkbox label="编辑" :checked="item[1]" @change="test(index,1)"></el-checkbox>
                 </el-form-item>
                 <el-form-item>
-                    <el-button size="mini" >确认修改</el-button>
+                    <el-button size="mini" @click="test">确认修改</el-button>
                 </el-form-item>
             </el-form>
-            <!-- <el-form :label-position="labelPosition" label-width="0px" :model="authorityForm" class="authForm">
-                <el-form-item>
-                    <p>dashboard1：</p>
-                    <div v-for="(item,index) in checkList" :key="index">
-                        <el-checkbox></el-checkbox>
-                        <span>echart {{item}}</span>
-                    </div>
-                </el-form-item>
-                <el-form-item>
-                    <el-button size="mini" >确认授权</el-button>
-                </el-form-item>
-            </el-form> -->
-           
-
         </el-main>
     </el-container>
 </template>
@@ -39,10 +23,17 @@
             return {
                 authorityForm: {},
                 labelPosition: 'left',
-                checkList: [1,2,3,4,5]
+                formList: [[false,true],[true,false],[true,false]]
             }
         },
+        mounted () {
+        },
         methods: {
+            test (index,checkNum) {
+                console.log(index,checkNum)
+                this.formList[index][checkNum] = !this.formList[index][checkNum]
+                console.log(this.formList)
+            }
             
         }
     }
